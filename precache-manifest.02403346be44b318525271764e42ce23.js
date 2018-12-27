92885359b89b0ee24fa46b84bb96ff08bc811c37
Lars Sundman <lars.sundman@iki.fi>
2018-12-28 00:53:08 +0200

    Updates

---
+++ precache-manifest.02403346be44b318525271764e42ce23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05514c35743ce689c628",
+    "url": "/2018-interactive-web-apps-project-lsundman/static/css/main.50277ed6.chunk.css"
+  },
+  {
+    "revision": "05514c35743ce689c628",
+    "url": "/2018-interactive-web-apps-project-lsundman/static/js/main.05514c35.chunk.js"
+  },
+  {
+    "revision": "d2d8bbf5e98513a91254",
+    "url": "/2018-interactive-web-apps-project-lsundman/static/js/1.d2d8bbf5.chunk.js"
+  },
+  {
+    "revision": "e0c2f665e2354f816a3b",
+    "url": "/2018-interactive-web-apps-project-lsundman/static/js/runtime~main.e0c2f665.js"
+  },
+  {
+    "revision": "d1cf5196ca26cb39c11a3a1ce533348a",
+    "url": "/2018-interactive-web-apps-project-lsundman/index.html"
+  }
+];
