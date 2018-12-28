70c5f7159a833c3ea8d0714f19504131c1c43aef
Lars Sundman <lars.sundman@iki.fi>
2018-12-28 03:06:56 +0200

    Updates

--- precache-manifest.e1a01c537415cd4567e66fa9522026db.js
+++ precache-manifest.e1a01c537415cd4567e66fa9522026db.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "e08a345f74ee9304adfe",
+    "revision": "f2631e6402f1374b2cb5",
     "url": "/2018-interactive-web-apps-project-lsundman/static/css/main.733f22c1.chunk.css"
   },
   {
-    "revision": "e08a345f74ee9304adfe",
-    "url": "/2018-interactive-web-apps-project-lsundman/static/js/main.e08a345f.chunk.js"
+    "revision": "f2631e6402f1374b2cb5",
+    "url": "/2018-interactive-web-apps-project-lsundman/static/js/main.f2631e64.chunk.js"
   },
   {
     "revision": "d2d8bbf5e98513a91254",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/2018-interactive-web-apps-project-lsundman/static/js/runtime~main.e0c2f665.js"
   },
   {
-    "revision": "6841fe94be14a58050061e60733b6bbb",
+    "revision": "0a7839c7968680560fd09b633b4eb2c6",
     "url": "/2018-interactive-web-apps-project-lsundman/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/2018-interactive-web-apps-project-lsundman/precache-manifest.ac6021281b409e4838faef9c822ba3b2.js"
+  "/2018-interactive-web-apps-project-lsundman/precache-manifest.e1a01c537415cd4567e66fa9522026db.js"
 );
 
 workbox.clientsClaim();
